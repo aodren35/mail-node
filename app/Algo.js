ca132464c5839bc804eb274c3b3d39f212dd5a83
aodren35 <aodren.letellier@etudiant.univ-rennes1.fr>
2018-01-23 12:36:25 +0100

    add début traitement grammaire

--- app/Algo.js
+++ app/Algo.js
@@ -10,33 +10,74 @@ var emlformat = require('eml-format');
 
 module.exports = function (app, loggerFile) {
     console.log("running");
-    var dataObj = {};
-    fs.writeFile('sample.json', '', function(){console.log('done')});
-    function readFiles(dirname, onFileContent, onError) {
+    var dataObj = [];
+    var obj = {};
+    var counter = 0;
+    function readFiles(dirname, onFileContent, callBackRead, onError) {
         fs.readdir(dirname, function(err, filenames) {
             if (err) {
                 onError(err);
                 return;
             }
-            filenames.forEach(function(filename) {
-                fs.readFile(dirname + filename, 'utf-8', function(err, content) {
-                    if (err) {
-                        onError(err);
-                        return;
-                    }
-                    onFileContent(filename, content);
+            obj = JSON.parse(fs.readFileSync('common/grammaire.json', 'utf8'));
+                filenames.forEach(function(filename) {
+                    fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+                        if (err) {
+                            onError(err);
+                            return;
+                        }
+                        onFileContent(filename, content);
+
+                        callBackRead(filenames.length);
+                    });
                 });
-            });
-        });
-    }
-    readFiles('./test/', function(filename, content) {
-        dataObj[filename] = content;
-        emlformat.read(content, function(error, data) {
-            if (error) return console.log(error);
-            fs.appendFileSync("sample.json", JSON.stringify(data, " ", 2));
         });
 
-    }, function(err) {
+    }
+    readFiles('./EML/', function(filename, content) {
+        emlformat.read(content, function(error, data) {
+            if (error) return console.log(error);
+            else {
+                // fs.appendFileSync("sample.json", JSON.stringify(data, ",", 2));
+                if (isOk(data)){
+                    var temp = {
+                        filename: data
+                    };
+                    dataObj.push(temp);
+                }
+                else {
+                    // console.log(data);
+                }
+
+                counter ++;
+            }
+        });
+
+    },
+        function(l) {
+        if (counter === l) {
+            fs.writeFile('sample.json', '', function () {
+                console.log('done ', dataObj.length);
+            });
+
+        }
+            },
+        function(err) {
         throw err;
     });
+
+
+    function isOk(data) {
+        if (data.text) {
+
+        }
+        else {
+            if (data.html) {
+
+            }
+            else {
+                return false;
+            }
+        }
+    }
 };
